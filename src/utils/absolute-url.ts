6ca5ac28242cc206bf333276c045da9adf6a1be5
Nathan Smith <nathan@nathan-smith.org>
2024-12-31 20:30:07 -0500

    ✨ feat: Add dynamic "tagged" pages

--- src/utils/absolute-url.ts
+++
@@ -1,6 +0,0 @@
-export function absoluteUrl(
-  value: string,
-  base: string = "https://nathan-smith.org"
-) {
-  return new URL(value, base).href;
-}
--- src/utils/active-route.ts
+++
@@ -1,9 +0,0 @@
-export function activeRoute(value: string, baseRoute: string): boolean {
-  if (
-    (baseRoute === "/" && value.length < 2) ||
-    (baseRoute !== "/" && value.startsWith(baseRoute))
-  ) {
-    return true;
-  }
-  return false;
-}
